1e84cb11872131b0a10c973a71e0824122abf47e
MD Halal Uddin <mdhelaluddin505@gmail.com>
2024-11-19 14:56:04 +0600

    function added

---
+++ src/1.6.ts
@@ -0,0 +1,25 @@
+//learn function
+// normal function
+//array function
+
+function add(num1: number, num2: number = 2): number {
+  return num1 + num2;
+}
+add(2 + 2);
+
+const arrayFunction = (num1: number, num2: number = 1): number => num1 + num2;
+arrayFunction(2 + 2);
+
+//object -------> function method
+
+const poorUser = {
+  name: "Halal Uddin",
+  balance: 0,
+  addBalance(balance: number): string {
+    return `My new balance is: ${this.balance + balance}`;
+  },
+};
+
+const arr: number[] = [1, 2, 3];
+
+const newArr = arr.map((elem: number): number => elem * elem);
