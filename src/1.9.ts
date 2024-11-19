2e02c68f3c95932bc3668adb804e0975fcd06cfc
MD Halal Uddin <mdhelaluddin505@gmail.com>
2024-11-19 17:33:35 +0600

    type alias

---
+++ src/1.9.ts
@@ -0,0 +1,30 @@
+{
+  //type Alias
+
+  type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo?: string;
+  };
+
+  const student1: Student = {
+    name: "Halal uddin",
+    age: 25,
+    gender: "male",
+    contactNo: "0181300000",
+  };
+  const student2: Student = {
+    name: "belal uddin",
+    age: 50,
+    gender: "male",
+    contactNo: "0130300000",
+  };
+  const student3: Student = {
+    name: "Roksana",
+    age: 35,
+    gender: "Female",
+  };
+
+  //
+}
